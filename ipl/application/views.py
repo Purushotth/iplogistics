ceb5cdd13abfde8303c07ae160d78e1986d957f2
Purushotth <purushcs70@gmail.com>
2021-12-19 21:33:18 +0530

    commented unwanted lines

--- ipl/application/views.py
+++ ipl/application/views.py
@@ -50,24 +50,23 @@ import html
 #     return FileResponse(open('tuto1.pdf', 'rb'))
 
 from fpdf import FPDF
-import xhtml2pdf
-import wkhtmltopdf
+
 
 from django.http import HttpResponse
 from django.template.loader import get_template
-from xhtml2pdf import pisa
-
-
-def render_to_pdf(template_src, context_dict={}):
-    template = get_template(template_src)
-    html = template.render(context_dict)
-    response = HttpResponse(content_type='application/pdf')
-    pdf_status = pisa.CreatePDF(html, dest=response)
-
-    if pdf_status.err:
-        return HttpResponse('Some errors were encountered <pre>' + html + '</pre>')
-
-    return response
+# from xhtml2pdf import pisa
+#
+#
+# def render_to_pdf(template_src, context_dict={}):
+#     template = get_template(template_src)
+#     html = template.render(context_dict)
+#     response = HttpResponse(content_type='application/pdf')
+#     pdf_status = pisa.CreatePDF(html, dest=response)
+#
+#     if pdf_status.err:
+#         return HttpResponse('Some errors were encountered <pre>' + html + '</pre>')
+#
+#     return response
 
 class PDF(FPDF):
     def lines(self):
