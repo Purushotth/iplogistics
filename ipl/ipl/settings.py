2246681b4a1869f4551a24bc310629d4e603de55
Purushotth <purushcs70@gmail.com>
2022-03-02 17:04:16 +0530

    wrong settings pushed

--- ipl/ipl/settings.py
+++ ipl/ipl/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'ipl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'ipl',
-#         'USER': 'root',
-#         'PASSWORD': 'Ipl0g!st!cs',
-#         'HOST': 'db',
-#         'PORT': 3306
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ipl',
+        'USER': 'root',
+        'PASSWORD': 'Ipl0g!st!cs',
+        'HOST': 'db',
+        'PORT': 3306
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
